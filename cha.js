fff4ff1687067d6bf5c8cf5e783352cd71770296
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-05-15 13:43:19 +0100

    dashboard login trial

---
+++ cha.js
@@ -0,0 +1,53 @@
+// 1 BMI challenge Javascript
+// const first = prompt("Enter first Number");
+// const second = prompt("Enter Second Number");
+
+// const calBMI = (weight, height) => {
+//   const BMIValue = weight / height;
+//   console.log("ye! :" + BMIValue);
+// };
+
+// calBMI(first, second);
+
+// 2 FizzBuzz challenge Javascript
+
+// const ChildrenPlayer = [];
+// let count = 1;
+
+// for (i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     ChildrenPlayer.push("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     ChildrenPlayer.push("Fizz");
+//   } else if (i % 5 === 0) {
+//     ChildrenPlayer.push("Buzz");
+//   } else {
+//     ChildrenPlayer.push(i);
+//   }
+//   count++;
+//   console.log(ChildrenPlayer);
+// }
+
+// 3 WhoIsBuying Lunch challenge Javascript
+// const names = [
+//   "Olarewaju",
+//   "Si feng",
+//   "Wu zhu si",
+//   "Blyncnov",
+//   "Mustapha",
+//   "Osiwin Faruk",
+//   "Babatunde",
+// ];
+
+// const ChooseWhoToPay = () => {
+//   const Payeer = names[Math.floor(Math.random() * names.length)];
+//   return console.log(Payeer + " " + "is going to buy Lunch Today");
+// };
+
+// ChooseWhoToPay();
+
+// 4 Fibonacci challenge Javascript
+
+const FibonacciArray = [];
+
+const fibonacci = () => {};
--- components/auth/Login/index.jsx
+++ components/auth/Login/index.jsx
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import axios from "axios";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
+  const [error, setError] = useState("");
+
   const SubmitHandler = async (e) => {
     e.preventDefault();
 
     const matricNo = e.target.matricNo.value;
     const password = e.target.password.value;
 
-    const response = await fetch(
-      "https://ict-chain-01.herokuapp.com/Authentication/student/login",
-      {
-        method: "POST",
-        credentials: "include",
-        headers: { "Content-Type": "application/json; charset=utf-8" },
-        body: JSON.stringify({
-          matricNo,
-          password,
-        }),
-      }
-    );
-    console.log(response);
+    // console.log(matricNo, password);
+
+    axios
+      .post("https://ict-chain-01.herokuapp.com/Authentication/student/login", {
+        matricNo: matricNo,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response.message);
+      })
+      .catch((error) => {
+        console.log(error);
+        // console.log(error.response.data.request.description);
+        // localStorage.setItem("error", error.response.data.request.description);
+        setError(error.message);
+        toast.error(error.message, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      });
   };
 
   return (
     <>
+      <ToastContainer />
       <div className="login__container ">
         <div className="bg">
           <h1>Student Login</h1>
@@ -33,10 +52,11 @@ const Login = () => {
           <div className="login__container__box">
             <h5> Sign in with credentials</h5>
             <br />
+            <h4 className="error__message">{error}</h4>
             <form onSubmit={SubmitHandler}>
               <input
                 className="form-control"
-                type="text"
+                type="number"
                 placeholder="matricNo"
                 name="matricNo"
                 // value={username}
@@ -50,12 +70,13 @@ const Login = () => {
                 name="password"
                 // value={password}
                 required
-                minLength={8}
+                minLength={5}
                 onChange={(e) => e.target.value}
                 placeholder="Password"
               />
               <br />
               <br />
+
               <div className="checkbox-crl">
                 <input className="checkbox-control" type="checkbox" />
                 <span>Remember me</span>
